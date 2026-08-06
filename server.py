f7c75f47913cb169d8e7dd15b983c48df8e8fed6
Khun <khunly@bstorm.be>
2026-08-06 14:51:40 +0200

    Refactor mail sending functionality to use Mailer service and streamline message handling

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
-from fastapi import Body, FastAPI, Query
-from fastapi_mail import ConnectionConfig, MessageSchema, FastMail
+from fastapi import Body, Depends, FastAPI, Query
 import uvicorn
 from dotenv import load_dotenv
 import os
@@ -9,6 +8,7 @@ import os
 from dto.hello_request_dto import HelloRequestDto
 from dto.hello_response_dto import HelloResponseDto
 from dto.mail_request_dto import MailRequestDto
+from services.mailer import Mailer
 
 load_dotenv()
 
@@ -30,29 +30,19 @@ def hello(
     )
     # return { 'result': f'Hello {dto.name * dto.nb}', 'square': dto.nb**2 }
 
+
 @app.post('/mail', status_code=201)
-async def mail(dto: MailRequestDto = Body()) -> None:
-    config = ConnectionConfig(
-        MAIL_SERVER=os.getenv('SMTP_HOST'),
-        MAIL_PORT=int(os.getenv('SMTP_PORT')),
-        MAIL_USERNAME=os.getenv('SMTP_USER'),
-        MAIL_PASSWORD=os.getenv('SMTP_PASS'),
-        MAIL_SSL_TLS=False,
-        MAIL_STARTTLS=False,
-        MAIL_FROM='admin@admin.be',
-        TEMPLATE_FOLDER = Path(__file__).parent / 'templates'
+async def mail(
+    dto: MailRequestDto = Body(),
+    mailer: Mailer = Depends(Mailer)
+) -> None:
+    
+    await mailer.send_message(
+        dto.subject,
+        dest=['lykhun@gmail.com'],
+        template_body=dict(dto),
+        template_name='mail_template.html'
     )
-    message = MessageSchema(
-        subject=dto.subject,
-        from_email='admin@admin.be',
-        reply_to=[dto.email],
-        recipients=['lykhun@gmail.com'],
-        template_body=dto.__dict__,
-        # body=f'L\'utilisateur {dto.name} ({dto.email}): Ce mail contenait: {dto.content}',
-        subtype='html'
-    )
-    mailer = FastMail(config)
-    await mailer.send_message(message, template_name='mail_template.html')
 
 if __name__ == '__main__':
     # exposer FastAPI sur le port 8000
@@ -62,3 +52,6 @@ if __name__ == '__main__':
         port=8000,
         reload=True
     )
+
+
+assert mail() == None
---
+++ services/mailer.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from fastapi_mail import FastMail, ConnectionConfig, MessageSchema
+
+load_dotenv()
+
+class Mailer():
+    config = ConnectionConfig(
+        MAIL_SERVER=os.getenv('SMTP_HOST'),
+        MAIL_PORT=int(os.getenv('SMTP_PORT')),
+        MAIL_USERNAME=os.getenv('SMTP_USER'),
+        MAIL_PASSWORD=os.getenv('SMTP_PASS'),
+        MAIL_SSL_TLS=False,
+        MAIL_STARTTLS=False,
+        MAIL_FROM='admin@admin.be',
+        TEMPLATE_FOLDER = Path(__file__).parent.parent / 'templates'
+    )
+        
+    async def send_message(self, subject: str, dest: list[str], template_body: dict, template_name: str):
+        message = MessageSchema(
+            subject=subject,
+            from_email='admin@admin.be',
+            recipients=dest,
+            template_body=template_body,
+            # body=f'L\'utilisateur {dto.name} ({dto.email}): Ce mail contenait: {dto.content}',
+            subtype='html'
+        )
+        mailer = FastMail(Mailer.config)
+        return await mailer.send_message(message, template_name=template_name)
