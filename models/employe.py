436be64388ce1087c899c9586a1c9a0fd2f7d6c4
elsarrive <elsaringling@gmail.com>
2026-08-10 16:14:07 +0200

    ajouts de contraintes not null

--- models/employe.py
+++ models/employe.py
@@ -14,12 +14,12 @@ class Employe(Base):
         DEV = auto()
         
     employee_id : Mapped[int] = mapped_column(primary_key=True)
-    last_name : Mapped[str] = mapped_column()
-    first_name : Mapped[str] = mapped_column()
+    last_name : Mapped[str] = mapped_column(nullable=False)
+    first_name : Mapped[str] = mapped_column(nullable=False)
     email : Mapped[str] = mapped_column()
     salary : Mapped[float] = mapped_column(NUMERIC(7,2))
     titre : Mapped[Titre] = mapped_column(SQLEnum(Titre), nullable=False)
-    supervisor_id : Optional[Mapped[int]] = mapped_column(ForeignKey('employe.employee_id'))
-    
+    supervisor_id : Mapped[Optional[int]] = mapped_column(ForeignKey('employe.employee_id'))
+
     superviseur : Mapped[Optional["Employe"]] = relationship("Employe", remote_side=[employee_id], back_populates='employees')
-    employees : Mapped[list["Employe"]] = relationship("Employe", back_populates='superviseur')
+    employees : Mapped[Optional[list["Employe"]]] = relationship("Employe", back_populates='superviseur')
